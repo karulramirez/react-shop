499a718cd359d17b49967143b80da5707f4ea59e
karul ramirez castillo <ramirezcastillokarul@gmail.com>
2022-10-02 16:18:14 -0500

    pratica3

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { default: dist } = require('mini-css-extract-plugin/dist');
 
 module.exports = {
 	entry: './src/index.js',
@@ -7,6 +9,7 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js',
 	},
+	mode: 'development',
 	resolve: {
 		extensions: ['.js', '.jsx'],
 	},
@@ -26,6 +29,14 @@ module.exports = {
 						loader: 'html-loader'
 					}
 				]
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					"style-loader",
+					"css-loader",
+					"sass-loader",
+				],
 			}
 		]
 	},
@@ -34,5 +45,8 @@ module.exports = {
 			template: './public/index.html',
 			filename: './index.html'
 		}),
-	]
+		new MiniCssExtractPlugin({
+			filename: '[name].css'
+		}),
+	],
 }
